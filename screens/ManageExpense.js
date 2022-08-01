8a033e48980c9d750575a2e6c6b0bae44d7a70c5
Kim Wauben <72084242+KimW1998@users.noreply.github.com>
2022-08-01 16:59:18 +0200

    made it possible to add a item with data and even edit but no old text shows yet

--- screens/ManageExpense.js
+++ screens/ManageExpense.js
@@ -28,19 +28,11 @@ function ManageExpense({ route, navigation }) {
     navigation.goBack();
   }
 
-  function confirmHandler() {
+  function confirmHandler(expenseData) {
     if (isEditing) {
-      expensesCtx.updateExpense(editedExpenseId, {
-        description: "Test!!!!",
-        amount: 29.99,
-        date: new Date("2022-05-20"),
-      });
+      expensesCtx.updateExpense(editedExpenseId, expenseData);
     } else {
-      expensesCtx.addExpense({
-        description: "Test",
-        amount: 19.99,
-        date: new Date("2022-05-19"),
-      });
+      expensesCtx.addExpense(expenseData);
     }
     navigation.goBack();
   }
@@ -50,6 +42,7 @@ function ManageExpense({ route, navigation }) {
       <ExpenseForm
         onCancel={cancelHandler}
         submitButtonLabel={isEditing ? "Update" : "Add"}
+        onSubmit={confirmHandler}
       />
 
       {isEditing && (
